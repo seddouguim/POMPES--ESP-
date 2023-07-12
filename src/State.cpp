95f119b08710b2284a30b4c5caeb3f3ad728ea3f
El Mehdi Seddougui <seddouguim@gmail.com>
2023-07-12 13:33:38 +0200

    New calibration method (EMA)

--- src/State.cpp
+++ src/State.cpp
@@ -43,88 +43,112 @@ void State::init()
 
 float State::get_calibrated_temperature()
 {
+    const int numReadings = 10; // Number of readings to average
+    float sum = 0.0;
 
-    // MAX31855 thermocouple voltage reading in mV
-    float thermocoupleVoltage = (Thermocouple_max31855.readCelsius() - Thermocouple_max31855.readInternal()) * 0.041276;
-
-    // MAX31855 cold junction voltage reading in mV
-    float coldJunctionTemperature = Thermocouple_max31855.readInternal();
-    float coldJunctionVoltage = -0.176004136860E-01 +
-                                0.389212049750E-01 * coldJunctionTemperature +
-                                0.185587700320E-04 * pow(coldJunctionTemperature, 2.0) +
-                                -0.994575928740E-07 * pow(coldJunctionTemperature, 3.0) +
-                                0.318409457190E-09 * pow(coldJunctionTemperature, 4.0) +
-                                -0.560728448890E-12 * pow(coldJunctionTemperature, 5.0) +
-                                0.560750590590E-15 * pow(coldJunctionTemperature, 6.0) +
-                                -0.320207200030E-18 * pow(coldJunctionTemperature, 7.0) +
-                                0.971511471520E-22 * pow(coldJunctionTemperature, 8.0) +
-                                -0.121047212750E-25 * pow(coldJunctionTemperature, 9.0) +
-                                0.118597600000E+00 * exp(-0.118343200000E-03 *
-                                                         pow((coldJunctionTemperature - 0.126968600000E+03), 2.0));
-
-    // cold junction voltage + thermocouple voltage
-    float voltageSum = thermocoupleVoltage + coldJunctionVoltage;
-
-    // calculate corrected temperature reading based on coefficients for 3 different ranges
-    float b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
-    if (thermocoupleVoltage < 0)
+    for (int i = 0; i < numReadings; i++)
     {
-        b0 = 0.0000000E+00;
-        b1 = 2.5173462E+01;
-        b2 = -1.1662878E+00;
-        b3 = -1.0833638E+00;
-        b4 = -8.9773540E-01;
-        b5 = -3.7342377E-01;
-        b6 = -8.6632643E-02;
-        b7 = -1.0450598E-02;
-        b8 = -5.1920577E-04;
-        b9 = 0.0000000E+00;
+        // MAX31855 thermocouple voltage reading in mV
+        float thermocoupleVoltage = (Thermocouple_max31855.readCelsius() - Thermocouple_max31855.readInternal()) * 0.041276;
+        sum += thermocoupleVoltage;
+        delay(10); // Add a small delay between readings to stabilize the sensor
     }
 
-    else if (thermocoupleVoltage < 20.644)
-    {
-        b0 = 0.000000E+00;
-        b1 = 2.508355E+01;
-        b2 = 7.860106E-02;
-        b3 = -2.503131E-01;
-        b4 = 8.315270E-02;
-        b5 = -1.228034E-02;
-        b6 = 9.804036E-04;
-        b7 = -4.413030E-05;
-        b8 = 1.057734E-06;
-        b9 = -1.052755E-08;
-    }
+    float averageVoltage = sum / numReadings;
 
-    else if (thermocoupleVoltage < 54.886)
-    {
-        b0 = -1.318058E+02;
-        b1 = 4.830222E+01;
-        b2 = -1.646031E+00;
-        b3 = 5.464731E-02;
-        b4 = -9.650715E-04;
-        b5 = 8.802193E-06;
-        b6 = -3.110810E-08;
-        b7 = 0.000000E+00;
-        b8 = 0.000000E+00;
-        b9 = 0.000000E+00;
-    }
+    // Calculate the Exponential Moving Average (EMA)
+    const float smoothingFactor = 0.2;         // Smoothing factor (0 < smoothingFactor < 1)
+    static float previousEMA = averageVoltage; // Initial EMA value
 
+    float currentEMA = smoothingFactor * averageVoltage + (1 - smoothingFactor) * previousEMA;
+    previousEMA = currentEMA; // Update the previous EMA value
+
+    // Rest of the calibration code using the EMA value
+    float calibratedTemperature = 0.0;
+
+    // Use the EMA value to perform the temperature calibration
+    if (currentEMA < 0)
+    {
+        // Calibration coefficients for negative thermocouple voltage
+        float b0 = 0.0000000E+00;
+        float b1 = 2.5173462E+01;
+        float b2 = -1.1662878E+00;
+        float b3 = -1.0833638E+00;
+        float b4 = -8.9773540E-01;
+        float b5 = -3.7342377E-01;
+        float b6 = -8.6632643E-02;
+        float b7 = -1.0450598E-02;
+        float b8 = -5.1920577E-04;
+        float b9 = 0.0000000E+00;
+
+        calibratedTemperature = b0 +
+                                b1 * currentEMA +
+                                b2 * pow(currentEMA, 2.0) +
+                                b3 * pow(currentEMA, 3.0) +
+                                b4 * pow(currentEMA, 4.0) +
+                                b5 * pow(currentEMA, 5.0) +
+                                b6 * pow(currentEMA, 6.0) +
+                                b7 * pow(currentEMA, 7.0) +
+                                b8 * pow(currentEMA, 8.0) +
+                                b9 * pow(currentEMA, 9.0);
+    }
+    else if (currentEMA < 20.644)
+    {
+        // Calibration coefficients for thermocouple voltage range 0 to 20.644 mV
+        float b0 = 0.000000E+00;
+        float b1 = 2.508355E+01;
+        float b2 = 7.860106E-02;
+        float b3 = -2.503131E-01;
+        float b4 = 8.315270E-02;
+        float b5 = -1.228034E-02;
+        float b6 = 9.804036E-04;
+        float b7 = -4.413030E-05;
+        float b8 = 1.057734E-06;
+        float b9 = -1.052755E-08;
+
+        calibratedTemperature = b0 +
+                                b1 * currentEMA +
+                                b2 * pow(currentEMA, 2.0) +
+                                b3 * pow(currentEMA, 3.0) +
+                                b4 * pow(currentEMA, 4.0) +
+                                b5 * pow(currentEMA, 5.0) +
+                                b6 * pow(currentEMA, 6.0) +
+                                b7 * pow(currentEMA, 7.0) +
+                                b8 * pow(currentEMA, 8.0) +
+                                b9 * pow(currentEMA, 9.0);
+    }
+    else if (currentEMA < 54.886)
+    {
+        // Calibration coefficients for thermocouple voltage range 20.644 to 54.886 mV
+        float b0 = -1.318058E+02;
+        float b1 = 4.830222E+01;
+        float b2 = -1.646031E+00;
+        float b3 = 5.464731E-02;
+        float b4 = -9.650715E-04;
+        float b5 = 8.802193E-06;
+        float b6 = -3.110810E-08;
+        float b7 = 0.000000E+00;
+        float b8 = 0.000000E+00;
+        float b9 = 0.000000E+00;
+
+        calibratedTemperature = b0 +
+                                b1 * currentEMA +
+                                b2 * pow(currentEMA, 2.0) +
+                                b3 * pow(currentEMA, 3.0) +
+                                b4 * pow(currentEMA, 4.0) +
+                                b5 * pow(currentEMA, 5.0) +
+                                b6 * pow(currentEMA, 6.0) +
+                                b7 * pow(currentEMA, 7.0) +
+                                b8 * pow(currentEMA, 8.0) +
+                                b9 * pow(currentEMA, 9.0);
+    }
     else
     {
-        // TODO: handle error - out of range
-        return 0;
+        // Handle error - out of range
+        calibratedTemperature = previous_temperature;
     }
 
-    return b0 +
-           b1 * voltageSum +
-           b2 * pow(voltageSum, 2.0) +
-           b3 * pow(voltageSum, 3.0) +
-           b4 * pow(voltageSum, 4.0) +
-           b5 * pow(voltageSum, 5.0) +
-           b6 * pow(voltageSum, 6.0) +
-           b7 * pow(voltageSum, 7.0) +
-           b8 * pow(voltageSum, 8.0) +
-           b9 * pow(voltageSum, 9.0);
+    return calibratedTemperature; // Return the calibrated temperature
 }
 
 void State::update()
